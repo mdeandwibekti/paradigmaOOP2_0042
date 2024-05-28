066238864eb89768d5a9888e5babe52359d649a4
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-28 18:52:53 +0700

    void setXY

--- abstraksi/abstraksi.cpp
+++ abstraksi/abstraksi.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+class abstraksiKlas {
+private:
+
+public:
+
+	//method untuk mengisi nilai
+	//private member
+	void setXY(string a, string b) {
+		x = a;
+		y = b;
+	}
+	//menampilkan nilai
+
+};
