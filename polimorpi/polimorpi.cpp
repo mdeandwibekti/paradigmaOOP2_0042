7dd37e7d5e02cb9f1af4569339213337d11c8860
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-28 17:49:59 +0700

    int main

--- polimorpi/polimorpi.cpp
+++ polimorpi/polimorpi.cpp
@@ -23,4 +23,18 @@ public:
 	}
 };
 
+int main() {
+	seseorang* obyek;
+	joko a;
+	lia b;
+
+	obyek = &a;
+	obyek->pesan();
+	obyek = &b;
+	obyek->pesan();
+	//a.pesan seseorang();
+
+	return 0;
+}
+
 
