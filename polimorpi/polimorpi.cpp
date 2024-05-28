dfdf056bbdefc6d7ddbe8dbf76d77d33f24a251f
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-28 17:38:09 +0700

    class seseorang

--- polimorpi/polimorpi.cpp
+++ polimorpi/polimorpi.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
 
-class seseorang{
+class seseorang {
 public:
+	virtual void pesan() = 0;
+	//virtual void pesan(){
+	// cout <<"pesan dari seseorang"<<endl;
+	//}
+};
 
-}
